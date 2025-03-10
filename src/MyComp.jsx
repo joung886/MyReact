a97687558f458cb83da0f5852eadd33da5cfce2c
Joung886 <rkdcjf311@naver.com>
2025-03-10 17:31:16 +0900

    Update MyComp.jsx

--- src/MyComp.jsx
+++ src/MyComp.jsx
@@ -10,13 +10,66 @@
 
 // export 키워드를 붙이면 외부 파일의 컴포넌트가 import 할 수 있음
 export function MyComp() {
+    const colors = ["black", "white", "blue"];
+    const [first, second] = colors; // 배열의 구조분해할당
+    const user = {
+        name : "Steve",
+        age : 25,
+    };
+    const { name, age } = user; // 객체의 구조분해할당 {} 중괄호사용
+    console.log(first + " " + second);
+    console.log(name + " " + age);
+
+    const array1 = [1,2,3];
+    const array2 = [4,5,6];
+    const array3 = [...array1, ...array2];
+    console.log(array3);
+    // 스프레드 연산자의 유용한 사용법
+    // 배열에 새로운 데이터를 추가하는데 항상 제일 앞에 추가
+    const array4 = [10, ...array3];
+    console.log(array4);
+    const array5 = [...array4, 100]; // 제일 뒤에추가
+    console.log(array5);
+    // 참조복사가 아닌 값복사를 할 수 있음
+    const array6 = array1; // 참조복사
+    const array7 = [...array1]; // 값복사
+    console.log(array6); // [1,2,3]
+    array1[0] = 10;
+    console.log(array6) // [10,2,3]
+    console.log(array7) // [1,2,3]
+
+    const numbers = [1,2,3];
+    const mapResult = numbers.map((num) => num*2);
+    console.log(mapResult); // 2,4,6
+    const filterResult = numbers.filter((num) => num % 2 === 0);
+    console.log(filterResult); // 2
+    const users = [
+        { id: 1, name: "Kim" },
+        { id: 2, name: "Lee" },
+    ];
+    const findResult = users.find((user) => user.id === 2);
+    console.log(findResult);
+
+    function method1(x) {
+        console.log("method1함수의 출력 : " + x)
+    }
+
+    const isLoggedIn = true;
+
     return(
     <>
+        <p>나의 컴포넌트</p>
         <MyCompOnly />
+        <div onClick={() => method1("1000")}> 여기를 클릭하시오! </div>
+        {isLoggedIn ? <p>당신은 로그인상태입니다.</p> : <p>로그인해주세요.</p>}
     </>
     );
 }
 // export 키워드가 없으면 같은 파일에서만 사용할 수 있음
 function MyCompOnly() {
-    return<></>;
+    return (
+    <>
+        <p>내부 컴포넌트</p>
+    </>    
+    );
 }
